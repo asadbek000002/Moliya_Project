873d5cfda12bb795ffd39f57709039c1244b9f4c
Rustambek <rustamsharipov393@gmail.com>
2023-10-14 11:02:09 -0700

    PaymentCost tayyor

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Employee, Course, UserContract
+from .models import Employee, Course, UserContract, PaymentCost
 
 
 @admin.register(Employee)
@@ -12,6 +12,18 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'image', 'description', 'continuity', 'number_of_people', 'course_program', 'offer']
 
 
+class PaymentCostInline(admin.TabularInline):
+    model = PaymentCost
+    raw_id_fields = ['contract']
+
+
 @admin.register(UserContract)
 class UserContractAdmin(admin.ModelAdmin):
     list_display = ['user', 'address']
+    inlines = [PaymentCostInline]
+
+
+@admin.register(PaymentCost)
+class PaymentCostAdmin(admin.ModelAdmin):
+    list_display = ['price', 'created', 'contract']
+
